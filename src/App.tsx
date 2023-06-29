d298a3b6fbbcdd5979260166eee221842d06a202
Juanjo Ortiz <jjortiz@ilernaonline.com>
2023-06-29 10:33:09 +0200

    finished selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import SelectSearch from './components/SelectSearch/SelectSearch'
+import { useState } from 'react'
 
 function App() {
+  const [val, setVal] = useState()
 
   return (
     <>
       <div style={{ width: '100%' }}>
-        <SelectSearch />
+        <SelectSearch onChange={(e) => setVal(e)} value={val} />
       </div>
     </>
   )
--- src/components/SelectSearch/SelectSearch.tsx
+++ src/components/SelectSearch/SelectSearch.tsx
@@ -1,9 +1,73 @@
 import './styles-select-search.scss'
+import { useEffect, useRef, useState } from 'react'
 
-export default function SelectSearch () {
+type Option = {
+  id: number,
+  name: string
+}
+
+const options: Option[] = [
+  {id: 1, name: 'Madrid'},
+  {id: 2, name: 'Sevilla'},
+  {id: 3, name: 'Malaga'},
+]
+
+interface Props {
+  value: Option,
+  onChange: (Option) => void
+}
+
+export default function SelectSearch ({ value, onChange }: Props) {
+  const [showSelector, setShowSelector] = useState(false)
+  const [inputSearch, setInputSearch] = useState('')
+  const [ optionsArray, setOptionsArray ] = useState( options || [] )
+  const inputRef = useRef<any>()
+  const selectorRef = useRef<any>()
+
+  useEffect(() => {
+    if (value) setInputSearch(value.name)
+  }, [value])
+
+  useEffect(() => {
+    const closeSelector = (e: MouseEvent) => {
+      const elementClicked = e.composedPath().find((element) => inputRef.current === element)
+      if (!elementClicked) setShowSelector(false)
+    }
+
+    if (showSelector) document.addEventListener('click', closeSelector)
+    return () => document.removeEventListener('click', closeSelector)
+  }, [showSelector])
+
+  const handleTextSearch = (e) => {
+    const textToSearch = e.target.value
+    setInputSearch(textToSearch)
+
+    const optionsFiltered = options.filter((option: Option) => {
+      const regex = new RegExp(textToSearch, 'gi')
+      return option.name.match(regex)
+    })
+
+    setOptionsArray(optionsFiltered)
+  }
+
+  const handleFocusSearch = () => {
+    setShowSelector(true)
+  }
+
+  const handleSelect = (option) => {
+    onChange(option)
+  }
+
   return (
     <div className='select-search'>
-      SelectSearch
+      <div className='input-container'>
+        <input type="text" value={inputSearch} onChange={handleTextSearch} onFocus={handleFocusSearch} ref={inputRef} />
+        { showSelector && optionsArray && (
+          <div className='selector' ref={selectorRef}>
+            {optionsArray.map(option => <div key={option.id} onClick={() => handleSelect(option)}>{option.name}</div> )}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
